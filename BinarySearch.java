76e91d4bbbd9ec97e6027b168f41c10a24e852fe
galsri03 <162865663+galsri03@users.noreply.github.com>
2025-04-16 14:48:21 +0000

    946-Initial-Commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,45 @@
 import java.util.Arrays;
 
+/* 
+Add them together and divide by two 
+
+integer/integer=integer
+
+1-1,000
+
+If the number is too low when sorting through an array add 1 to the search and make it the bottom number
+
+If the number is too high when sorting through an array minus 1 and make it the top number 
+
+If the bottom is greater than the top it wasn’t there
+
+Index of returns the position 
+
+A binary search is index of
+*/
+
 public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		int top = ray.length-1; 
+		int bottom = 0;
+
+	while (bottom<=top)
+	{
+		int middle = (bottom + top) / 2;
+
+		if(ray[middle]==item) 
+			return middle;
+
+		if(ray[middle]<item) 
+			bottom = middle + 1;
+
+		else //(ray[middle]>item) 
+			top = middle - 1;
+	}
+
+		return -1;
 
 	}
 }
--- BinarySearchTester.java
+++ BinarySearchTester.java
@@ -9,10 +9,16 @@ public class BinarySearchTester
 		System.out.println("before calling binarySearch");
 		System.out.println(Arrays.toString(array));
 
+		// to avoid printing only negative ones and sort in ascending order, i used array.sort
+		// Bnary searching soley works on an array that is sorted
+		Arrays.sort(array);
+		System.out.println("after sorting");
+		System.out.println(Arrays.toString(array));
+
 		System.out.println("looking for 8 :: "+	BinarySearch.binarySearch(array,8));
 		System.out.println("looking for 39 :: "+	BinarySearch.binarySearch(array,39));
 		System.out.println("looking for 15 :: "+	BinarySearch.binarySearch(array,15));
-		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5);
-		//System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10);
+		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5));
+		System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10));
 	}
 }
